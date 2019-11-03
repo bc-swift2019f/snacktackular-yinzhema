8419b2c1f202576bce0533c1df93c8205f2f0802
yinzhema <yma9826@gmail.com>
2019-11-03 13:33:24 -0500

    session 9.2 complete

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,13 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Yinzhe Ma on 11/3/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots{
+    var spotArray=[Spot]()
+}
