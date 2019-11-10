9db8d1210f08e914a001115ee6192bb1d606141f
yinzhema <yma9826@gmail.com>
2019-11-10 14:16:42 -0500

    session 9.11 complete

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -19,6 +19,8 @@ class SpotDetailViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var cancelBarButton: UIBarButtonItem!
     
     var spot:Spot!
     let regionDistance: CLLocationDistance=750//750 meters or about a half mile
@@ -27,16 +29,40 @@ class SpotDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tap=UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView=false
+        self.view.addGestureRecognizer(tap)
         //mapView.delegate=self
         if spot==nil{
             spot=Spot()
             getLocation()
+            nameField.addBorder(width: 0.5, radius: 5.0, color: .black)
+            addressField.addBorder(width: 0.5, radius: 5.0, color: .black)
+        } else{
+            nameField.isEnabled=false
+            addressField.isEnabled=false
+            nameField.backgroundColor=UIColor.clear
+            addressField.backgroundColor=UIColor.white
+            saveBarButton.title=""
+            cancelBarButton.title=""
+            navigationController?.setToolbarHidden(true, animated: true)
         }
         nameField.text=spot.name
         addressField.text=spot.address
         
         let region=MKCoordinateRegion(center: spot.coordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
         mapView.setRegion(region, animated: true)
+        updateUserInterface()
+    }
+    
+    @IBAction func textFieldEditingChange(_ sender: UITextField) {
+        saveBarButton.isEnabled = !(nameField.text=="")
+    }
+    
+    @IBAction func textFieldReturnPressed(_ sender: UITextField) {
+        sender.resignFirstResponder()
+        spot.name=nameField.text!
+        spot.address=addressField.text!
         updateUserInterface()
     }
     
--- Snacktacular/SpotsListViewController.swift
+++ Snacktacular/SpotsListViewController.swift
@@ -40,6 +40,7 @@ class SpotsListViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         getLocation()
+        navigationController?.setToolbarHidden(false, animated: false)
         spots.loadData {
             self.sortBasedOnSegmentPressed()
             self.tableView.reloadData()
