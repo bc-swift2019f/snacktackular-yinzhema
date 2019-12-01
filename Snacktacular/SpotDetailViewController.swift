6e1d2aeec1c2f6683db444e436c2e38bcc21dfa1
yinzhema <yma9826@gmail.com>
2019-12-01 14:17:50 -0500

    session 9.24 complete

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -72,6 +72,16 @@ class SpotDetailViewController: UIViewController {
         
         photos.loadData(spot: spot) {
             self.tableView.reloadData()
+            if self.reviews.reviewArray.count>0{
+                var total=0
+                for review in self.reviews.reviewArray{
+                    total=total+review.rating
+                }
+                let average=Double(total)/Double(self.reviews.reviewArray.count)
+                self.averageRatingLabel.text="\(average.roundTo(places: 1))"
+            } else{
+                self.averageRatingLabel.text = "-.-"
+            }
         }
         
         photos.loadData(spot: spot) {
